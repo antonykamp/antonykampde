103fd1fb2394ef9ede8be1c9b3e95d3e5d60479a
DuckNrOne <tony.kamp@web.de>
2021-03-26 10:19:23 +0100

    update sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://antonykamp.de',
-    generateRobotsTxt: true
+    generateRobotsTxt: true,
+    exclude: ['*impressum*','*datenschutz*']
 }
